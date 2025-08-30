f1bcfb29097a57a3e6bd4a6e1128d88c319fe77a
yy166431 <1664316064@qq.com>
2025-08-31 03:46:24 +0800

    Update fishhook.h

--- fishhook.h
+++ fishhook.h
@@ -1,6 +1,3 @@
-// fishhook.h — Copyright (c) Facebook, Inc.
-// 许可: MIT. 仅接口。
-
 #ifndef fishhook_h
 #define fishhook_h
 
@@ -11,12 +8,11 @@ extern "C" {
 #endif
 
 struct rebinding {
-  const char *name;     // 需要替换的符号名（去掉前导下划线）
-  void *replacement;    // 我们的实现
-  void **replaced;      // 用来保存原实现指针的地址
+  const char *name;
+  void *replacement;
+  void **replaced;
 };
 
-// 绑定一组符号（对当前和后续加载的 image 都生效）
 int rebind_symbols(struct rebinding rebindings[], size_t rebindings_nel);
 
 #ifdef __cplusplus
