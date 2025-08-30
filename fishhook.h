228dc0cf05876df2a2850526c6577ee43ec111f7
yy166431 <1664316064@qq.com>
2025-08-31 03:26:21 +0800

    Update fishhook.h

--- fishhook.h
+++ fishhook.h
@@ -1,22 +1,21 @@
-// fishhook.h
-// https://github.com/facebook/fishhook
+// fishhook.h  — from Facebook fishhook (trimmed interface)
 
 #ifndef fishhook_h
 #define fishhook_h
 
 #include <stddef.h>
-#include <stdint.h>
 
 #ifdef __cplusplus
 extern "C" {
 #endif
 
 struct rebinding {
-  const char *name;
-  void *replacement;
-  void **replaced;
+  const char *name;     // 要替换的符号名
+  void *replacement;    // 我们的实现
+  void **replaced;      // 保存原实现的指针地址
 };
 
+// 重新绑定一组符号
 int rebind_symbols(struct rebinding rebindings[], size_t rebindings_nel);
 
 #ifdef __cplusplus
