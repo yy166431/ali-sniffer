568376f1c89ec9a5d89a322942b051c8721943ba
yy166431 <1664316064@qq.com>
2025-09-25 18:17:27 +0800

    Update AliSniffer.m

--- AliSniffer.m
+++ AliSniffer.m
@@ -1,59 +1,116 @@
 // WxSniffer.m
+// TrollStore 注入微信，捕获直播源 (m3u8 / flv / rtmp)
+// 只弹窗显示并可复制，不写日志、不写文件
+
 #import <Foundation/Foundation.h>
+#import <objc/runtime.h>
 #import <UIKit/UIKit.h>
-#import "fishhook.h"
+#import <AVFoundation/AVFoundation.h>
 
-static NSURLSessionDataTask* (*orig_dataTaskWithRequest)(id self, SEL _cmd, NSURLRequest *request, void (^completionHandler)(NSData *, NSURLResponse *, NSError *));
+#pragma mark - Helper
+static void showStreamAlert(NSString *url, NSString *tag) {
+    static BOOL showed = NO;
+    if (showed) return;   // 避免重复刷屏
+    showed = YES;
 
-NSURLSessionDataTask* replaced_dataTaskWithRequest(id self, SEL _cmd, NSURLRequest *request, void (^completionHandler)(NSData *, NSURLResponse *, NSError *)) {
+    dispatch_async(dispatch_get_main_queue(), ^{
+        UIAlertController *alert = [UIAlertController alertControllerWithTitle:@"抓到直播源"
+                                                                       message:url
+                                                                preferredStyle:UIAlertControllerStyleAlert];
+        [alert addAction:[UIAlertAction actionWithTitle:@"复制" style:UIAlertActionStyleDefault handler:^(UIAlertAction * _Nonnull action) {
+            UIPasteboard.generalPasteboard.string = url;
+        }]];
+        [alert addAction:[UIAlertAction actionWithTitle:@"关闭" style:UIAlertActionStyleCancel handler:nil]];
+
+        UIViewController *rootVC = [UIApplication sharedApplication].keyWindow.rootViewController;
+        [rootVC presentViewController:alert animated:YES completion:nil];
+    });
+}
+
+#pragma mark - NSURLSession Hook
+@implementation NSURLSession (Sniffer)
+
++ (void)load {
+    static dispatch_once_t onceToken;
+    dispatch_once(&onceToken, ^{
+        Class cls = [NSURLSession class];
+        SEL origSel = @selector(dataTaskWithRequest:completionHandler:);
+        SEL newSel  = @selector(sniffer_dataTaskWithRequest:completionHandler:);
+
+        Method orig = class_getInstanceMethod(cls, origSel);
+        Method newM = class_getInstanceMethod(cls, newSel);
+
+        method_exchangeImplementations(orig, newM);
+    });
+}
+
+- (NSURLSessionDataTask *)sniffer_dataTaskWithRequest:(NSURLRequest *)request
+                                    completionHandler:(void (^)(NSData *, NSURLResponse *, NSError *))completionHandler {
     NSString *url = request.URL.absoluteString;
     if ([url containsString:@".m3u8"] ||
         [url containsString:@".flv"]  ||
         [url hasPrefix:@"rtmp://"]) {
-
-        NSLog(@"[WxSniffer] 捕获直播源: %@", url);
-
-        // 写入文件，避免只看日志
-        NSString *path = @"/var/mobile/Containers/Data/wx_stream_url.txt";
-        [url writeToFile:path atomically:YES encoding:NSUTF8StringEncoding error:nil];
-
-        // 弹窗（可选，避免刷屏只弹一次）
-        static BOOL showed = NO;
-        if (!showed) {
-            showed = YES;
-            dispatch_async(dispatch_get_main_queue(), ^{
-                UIAlertController *alert = [UIAlertController alertControllerWithTitle:@"抓到直播源"
-                                                                               message:url
-                                                                        preferredStyle:UIAlertControllerStyleAlert];
-                [alert addAction:[UIAlertAction actionWithTitle:@"复制" style:UIAlertActionStyleDefault handler:^(UIAlertAction * _Nonnull action) {
-                    UIPasteboard.generalPasteboard.string = url;
-                }]];
-                [alert addAction:[UIAlertAction actionWithTitle:@"关闭" style:UIAlertActionStyleCancel handler:nil]];
-                [[UIApplication sharedApplication].keyWindow.rootViewController presentViewController:alert animated:YES completion:nil];
-            });
-        }
+        showStreamAlert(url, @"NSURLSession");
     }
-    return orig_dataTaskWithRequest(self, _cmd, request, completionHandler);
+    return [self sniffer_dataTaskWithRequest:request completionHandler:completionHandler];
 }
 
-// 延迟初始化，避免微信刚启动时直接 hook 崩溃
-static void setupHook() {
+@end
+
+#pragma mark - AVPlayer Hook
+@implementation AVPlayer (Sniffer)
+
++ (void)load {
     static dispatch_once_t onceToken;
     dispatch_once(&onceToken, ^{
-        NSLog(@"[WxSniffer] 延迟初始化，准备 fishhook");
-        rebind_symbols((struct rebinding[1]){{
-            "dataTaskWithRequest:completionHandler:",
-            replaced_dataTaskWithRequest,
-            (void *)&orig_dataTaskWithRequest
-        }}, 1);
+        Class cls = [AVPlayer class];
+        SEL origSel = @selector(initWithURL:);
+        SEL newSel  = @selector(sniffer_initWithURL:);
+
+        Method orig = class_getInstanceMethod(cls, origSel);
+        Method newM = class_getInstanceMethod(cls, newSel);
+
+        method_exchangeImplementations(orig, newM);
     });
 }
 
-__attribute__((constructor))
-static void entry() {
-    NSLog(@"[WxSniffer] dylib 已注入，等待应用启动完成…");
-    // 延迟 3 秒再执行 hook
-    dispatch_after(dispatch_time(DISPATCH_TIME_NOW, (int64_t)(3 * NSEC_PER_SEC)), dispatch_get_main_queue(), ^{
-        setupHook();
+- (instancetype)sniffer_initWithURL:(NSURL *)URL {
+    NSString *url = URL.absoluteString;
+    if ([url containsString:@".m3u8"] ||
+        [url containsString:@".flv"]  ||
+        [url hasPrefix:@"rtmp://"]) {
+        showStreamAlert(url, @"AVPlayer");
+    }
+    return [self sniffer_initWithURL:URL];
+}
+
+@end
+
+#pragma mark - AVURLAsset Hook
+@implementation AVURLAsset (Sniffer)
+
++ (void)load {
+    static dispatch_once_t onceToken;
+    dispatch_once(&onceToken, ^{
+        Class cls = [AVURLAsset class];
+        SEL origSel = @selector(initWithURL:options:);
+        SEL newSel  = @selector(sniffer_initWithURL:options:);
+
+        Method orig = class_getInstanceMethod(cls, origSel);
+        Method newM = class_getInstanceMethod(cls, newSel);
+
+        method_exchangeImplementations(orig, newM);
     });
 }
+
+- (instancetype)sniffer_initWithURL:(NSURL *)URL options:(NSDictionary<NSString *,id> *)options {
+    NSString *url = URL.absoluteString;
+    if ([url containsString:@".m3u8"] ||
+        [url containsString:@".flv"]  ||
+        [url hasPrefix:@"rtmp://"]) {
+        showStreamAlert(url, @"AVURLAsset");
+    }
+    return [self sniffer_initWithURL:URL options:options];
+}
+
+@end
